18d0eb50a6c377e89e27940cb089a3beb641d1c3
amav1 <143661404+amav1@users.noreply.github.com>
2023-11-18 17:45:10 -0500

    Animated

--- main.py
+++ main.py
@@ -1,13 +1,46 @@
+
 import pygame
-#import your controller
+from Snow import Snowflake
+from random import randint
+from sys import exit
 
 def main():
-    pygame.init()
-    #Create an instance on your controller object
-    #Call your mainloop
-    
-    ###### NOTHING ELSE SHOULD GO IN main(), JUST THE ABOVE 3 LINES OF CODE ######
+	
+	pygame.init()
+	width, height = 1080, 580
+	screen = pygame.display.set_mode((width, height))
+	pygame.display.set_caption("Snow Simulation")
+	clock = pygame.time.Clock()
 
-# https://codefather.tech/blog/if-name-main-python/
-if __name__ == '__main__':
-    main()
+	trees_back = pygame.image.load('trees_back.jpg').convert_alpha()
+	trees_front = pygame.image.load('trees_front.png').convert_alpha()
+
+	
+	snowflakes = []
+	for i in range(0, 500):
+		position = (randint(0, width), randint(0, height))
+		gravity = randint(1, 2)
+		radius = randint(2, 4)
+		snowflakes.append(Snowflake(position, radius, gravity))
+
+	while True:
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				pygame.quit()
+				exit()
+
+		screen.blit(trees_back, (0, 0))		
+		
+		
+		for snowflake in snowflakes:
+			pygame.draw.circle(screen, snowflake.color, snowflake.pos, snowflake.radius)
+			snowflake.controller(height, width)
+
+		screen.blit(trees_front, (0, 0))	
+
+		pygame.display.update()
+		clock.tick(30)
+	
+
+if __name__ == "__main__":
+	main()
