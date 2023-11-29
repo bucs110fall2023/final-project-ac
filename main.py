996b286969e862a22dd70db834ed551caf529d56
Your Name <you@example.com>
2023-11-29 17:25:35 -0500

    yeehaw

--- main.py
+++ main.py
@@ -12,7 +12,6 @@ def main():
 	pygame.display.set_caption("Snow Simulation")
 	clock = pygame.time.Clock()
 
-
 	
 	snowflakes = []
 	for i in range(0, 500):
@@ -40,3 +39,21 @@ def main():
 
 if __name__ == "__main__":
 	main()
+
+	#from flask import Flask
+
+# app = Flask(__name__)
+
+# @app.route("/")
+# def hello_world():
+#     return "<p>Hello, World!</p>"
+
+# app = Flask(__name__)
+
+# @app.route("/")
+# def hello_world():
+#     return render_template("questionone.html")
+
+# @app.route("/two")
+# def hello_world():
+#     return render_template("questiontwo.html")
