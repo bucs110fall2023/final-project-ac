28421705f8492af505fb314a03b49997b4109cc6
Your Name <you@example.com>
2023-12-01 18:03:35 -0500

    Flask

--- myproject/app.py
+++ myproject/app.py
@@ -2,13 +2,12 @@ from flask import Flask, render_template
 import random
 
 app = Flask(__name__)
-
+app.debug = True  
 
 @app.route("/")
 def hello_world():
     var = random.choice(["Hello", "Goodbye"])
     return render_template("questionone.html", value=var)
-
 
 @app.route("/two")
 def two():
