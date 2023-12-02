0bdf89752c6aa5901155242880a09c6762782b96
Your Name <you@example.com>
2023-12-02 17:45:36 -0500

    Trial

--- Fuzzfeed/static/js/app.js
+++ Fuzzfeed/static/js/app.js
@@ -1,4 +1,4 @@
-// Assuming you have a container element in your HTML with the class "snowflakes-container"
+
 const container = document.querySelector('.snowflakes-container');
 
 for (var i = 0; i < 100; i++) {
@@ -11,15 +11,3 @@ for (var i = 0; i < 100; i++) {
     snowDiv.classList.add("snow");
 }
 
-
-for (var i = 0; i < 20; i++) {
-    let snowDiv = document.createElement("div");
-    snowDiv.classList.add("snowflake");
-    let image = document.createElement("img")
-    image.src = "./snowflake.svg"
-    snowDiv.appendChild(image)
-    snowDiv.style.animation = `snowfall ${Math.floor(Math.random() * 10) + 15}s ${Math.floor(Math.random() * 10)}s linear infinite`;
-    snowDiv.style.left = `${Math.floor(Math.random() * 100)}%`;
-    // For the other type of snowflake
-    snowDiv.classList.add("snowflake");
-}
