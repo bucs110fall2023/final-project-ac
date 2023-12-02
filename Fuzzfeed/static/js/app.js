74943c51387d43fc5059dacf8ce1e236899fd26a
amav1 <143661404+amav1@users.noreply.github.com>
2023-12-02 17:56:51 -0500

    Trial

--- Fuzzfeed/static/js/app.js
+++ Fuzzfeed/static/js/app.js
@@ -1,5 +1,4 @@
-
-const container = document.querySelector('.snowflakes-container');
+const container = document.querySelector('.snowflakes');
 
 for (var i = 0; i < 100; i++) {
     let snowDiv = document.createElement("div");
@@ -7,7 +6,7 @@ for (var i = 0; i < 100; i++) {
     snowDiv.style.animation = `snowfall ${Math.floor(Math.random() * 10) + 5}s ${Math.floor(Math.random() * 10)}s linear infinite`;
     snowDiv.style.left = `${Math.floor(Math.random() * 100)}%`;
 
-    // For the snowflakes
-    snowDiv.classList.add("snow");
+    container.appendChild(snowDiv); 
 }
+
 
