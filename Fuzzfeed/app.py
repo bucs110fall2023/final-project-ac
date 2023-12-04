f3da3cd67d0244ba2614a6e11d33e096cdcb5eda
amav1 <143661404+amav1@users.noreply.github.com>
2023-12-04 12:18:47 -0500

    Santa

--- Fuzzfeed/app.py
+++ Fuzzfeed/app.py
@@ -1,19 +1,18 @@
 from flask import Flask, render_template, send_from_directory
-import os
 
 app = Flask(__name__)
 
-# Route to serve the HTML page
+
 @app.route('/')
 def index():
     return render_template('startpage.html')
 
-# Route to serve static files (CSS, JS, images)
-@app.route('/src/<path:filename>')
+
+@app.route('/static/<path:filename>')
 def serve_static(filename):
-    src_folder = os.path.join(os.getcwd(), 'src')
-    return send_from_directory('src', filename)
+    return send_from_directory('static', filename)
 
 if __name__ == '__main__':
     app.run(debug=True)
+
 
