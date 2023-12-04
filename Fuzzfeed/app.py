d1b424161d21f24cc94bfdea9abda4b043c2044c
Your Name <you@example.com>
2023-12-03 19:36:08 -0500

    yuh

--- Fuzzfeed/app.py
+++ Fuzzfeed/app.py
@@ -10,6 +10,7 @@ def index():
 # Route to serve static files (CSS, JS, images)
 @app.route('/src/<path:filename>')
 def serve_static(filename):
+    src_folder = os.path.join(os.getcwd(), 'src')
     return send_from_directory('src', filename)
 
 if __name__ == '__main__':
