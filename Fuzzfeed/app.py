2298696ac222924c253d7ad6daf03c9ba2a1e13f
Your Name <you@example.com>
2023-12-04 13:38:49 -0500

    yuh

--- Fuzzfeed/app.py
+++ Fuzzfeed/app.py
@@ -1,7 +1,9 @@
 from flask import Flask, render_template, send_from_directory
 import os
+import logging
 
 app = Flask(__name__)
+logging.basicConfig(level=logging.DEBUG)
 
 # Route to serve the HTML page
 @app.route('/')
@@ -9,11 +11,10 @@ def index():
     return render_template('startpage.html')
 
 # Route to serve static files (CSS, JS, images)
-@app.route('/src/<path:filename>')
-def serve_static(filename):
-    src_folder = os.path.join(os.getcwd(), 'src')
-    return send_from_directory('src', filename)
+@app.route('/questionone')
+def question_one():
+    #src_folder = os.path.join(os.getcwd(), 'template')
+    return render_template('templates/questionone.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
-
